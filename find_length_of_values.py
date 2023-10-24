b16be96511421aa5f35cea4016448913015b65c7
allamurodxakimov <xakimov2806@gmail.com>
2023-10-24 13:00:25 -0700

    find length values

--- find_length_of_values.py
+++ find_length_of_values.py
@@ -6,4 +6,14 @@ def find_length_of_values(data: dict) -> int:
     Returns:
         int: The sum of the length of all values in the dictionary.
     """
-    return 
+    c=0
+    for i in data:
+        c+=len(data.get(i))
+        k=len(data.get(i))
+        print(data.get(i),"lenght = ",k)
+    return c
+print(find_length_of_values( data = {
+    'a': 'abc',
+    'b': 'def', 
+    'c': 'ghi'
+  }))
